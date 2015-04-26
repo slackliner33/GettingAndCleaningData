6eda65d84fa7478f0365363d24566d3c21a17180
slackliner33 <alpineslalomracer33@gmail.com>
2015-04-26 16:37:04 -0600

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,22 +1,21 @@
-# Source of data for this project: https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+#TO RUN, COPY ALL TEXT FILES FROM DATA SOURCE INTO THE FOLDER WHERE Rstudio RUNS FROM
 
-# This R script does the following:
 
-# 1. Merges the training and the test sets to create one data set.
+# Part 1 - Merging the training set and the test set to make one data set
 
-tmp1 <- read.table("train/X_train.txt")
-tmp2 <- read.table("test/X_test.txt")
-X <- rbind(tmp1, tmp2)
+temp1 <- read.table("X_train.txt")
+temp2 <- read.table("X_test.txt")
+X <- rbind(temp1, temp2)
 
-tmp1 <- read.table("train/subject_train.txt")
-tmp2 <- read.table("test/subject_test.txt")
-S <- rbind(tmp1, tmp2)
+temp1 <- read.table("subject_train.txt")
+temp2 <- read.table("subject_test.txt")
+S <- rbind(temp1, temp2)
 
-tmp1 <- read.table("train/y_train.txt")
-tmp2 <- read.table("test/y_test.txt")
-Y <- rbind(tmp1, tmp2)
+temp1 <- read.table("y_train.txt")
+temp2 <- read.table("y_test.txt")
+Y <- rbind(temp1, temp2)
 
-# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+#  Part 2 - Extracting only the measurements on the mean and std dev for each measurement
 
 features <- read.table("features.txt")
 indices_of_good_features <- grep("-mean\\(\\)|-std\\(\\)", features[, 2])
@@ -25,20 +24,20 @@ names(X) <- features[indices_of_good_features, 2]
 names(X) <- gsub("\\(|\\)", "", names(X))
 names(X) <- tolower(names(X))
 
-# 3. Uses descriptive activity names to name the activities in the data set.
+# Part 3 - Naming the activities in the data set
 
 activities <- read.table("activity_labels.txt")
 activities[, 2] = gsub("_", "", tolower(as.character(activities[, 2])))
 Y[,1] = activities[Y[,1], 2]
 names(Y) <- "activity"
 
-# 4. Appropriately labels the data set with descriptive activity names.
+# Part 4 - Labeling the data set with activity names
 
 names(S) <- "subject"
 cleaned <- cbind(S, Y, X)
 write.table(cleaned, "merged_clean_data.txt")
 
-# 5. Creates a 2nd, independent tidy data set with the average of each variable for each activity and each subject.
+# 5. Creating a second, independent tidy dataset with the ave of ea var for ea activity and subject
 
 uniqueSubjects = unique(S)[,1]
 numSubjects = length(unique(S)[,1])
@@ -56,4 +55,4 @@ for (s in 1:numSubjects) {
         row = row+1
     }
 }
-write.table(result, "data_set_with_the_averages.txt")
+write.table(result, "DataSetWithAverages.txt", row.names = FALSE)
